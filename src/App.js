811167f410c8394d98c70839bfaba113c7ebaf5c
Ramesh_Chathura <ramesh.crs96@gmail.com>
2021-01-08 18:43:25 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const blog = {
+    title: "My first blog",
+    description: "hello this is my first blog post",
+    author: "ramesh",
+  };
+
+  const styles = {
+    margin: "16px",
+    padding: "16px",
+    boxSizing: "border-box",
+    borderRadius: "5px",
+    textAlign: "center",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={styles}>
+        <h1>{blog.title}</h1>
+        <p>{blog.description}</p>
+        <i>{blog.author}</i>
+      </div>
+      <div style={styles}>
+        <h1>{blog.title}</h1>
+        <p>{blog.description}</p>
+        <i>{blog.author}</i>
+      </div>
+      <div style={styles}>
+        <h1>{blog.title}</h1>
+        <p>{blog.description}</p>
+        <i>{blog.author}</i>
+      </div>
     </div>
   );
 }
