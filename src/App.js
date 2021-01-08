ac07652a79e3ca217e2242c521547ed52edae320
Ramesh_Chathura <ramesh.crs96@gmail.com>
2021-01-08 23:21:37 +0530

    added cards

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import React from "react";
+import Card from "./card";
+import Card1 from "./components/card1";
 
 function App() {
-  const blog = {
-    title: "My first blog",
-    description: "hello this is my first blog post",
-    author: "ramesh",
-  };
-
-  const styles = {
-    margin: "16px",
-    padding: "16px",
-    boxSizing: "border-box",
-    borderRadius: "5px",
-    textAlign: "center",
-  };
-
   return (
-    <div className="App">
-      <div style={styles}>
-        <h1>{blog.title}</h1>
-        <p>{blog.description}</p>
-        <i>{blog.author}</i>
-      </div>
-      <div style={styles}>
-        <h1>{blog.title}</h1>
-        <p>{blog.description}</p>
-        <i>{blog.author}</i>
-      </div>
-      <div style={styles}>
-        <h1>{blog.title}</h1>
-        <p>{blog.description}</p>
-        <i>{blog.author}</i>
-      </div>
+    <div>
+      <Card />
+      <Card1 />
     </div>
   );
 }
---
+++ src/components/card1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Card1() {
+  return (
+    <div>
+      <h1>Oh! Finally</h1>
+    </div>
+  );
+}
+export default Card1;
