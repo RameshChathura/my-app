9ec968203bae5de4a512c60496b0cf9f552936f3
Ramesh <ramesh.crs96@gmail.com>
2021-01-09 12:24:39 +0530

    added new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Card from "./card";
 import Card1 from "./components/card1";
 
 function App() {
   return (
     <div>
-      <Card />
       <Card1 />
     </div>
   );
